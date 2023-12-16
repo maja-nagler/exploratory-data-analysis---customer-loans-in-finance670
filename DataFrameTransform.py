b7eb5778937b2791bdfd3ccc3bfa792a9e124a85
maja-nagler <2760739N@student.gla.ac.uk>
2023-12-16 22:03:15 +0000

    milestone 3

---
+++ DataFrameTransform.py
@@ -0,0 +1,53 @@
+
+
+import pandas as pd
+
+class DataFrameTransform:
+    def __init__(self, dataframe):
+        self.dataframe = dataframe
+
+    def null_percentage(self):
+        # Calculate percentage of NULL values in each column
+        return (self.dataframe.isnull().sum() / len(self.dataframe)) * 100
+
+    def drop_columns(self, columns):
+        # Drop specified columns
+        self.dataframe.drop(columns=columns, inplace=True)
+
+    def impute_nulls(self, columns, method='median'):
+        # Impute NULL values in specified columns using median or mean
+        for col in columns:
+            if method == 'median':
+                self.dataframe[col].fillna(self.dataframe[col].median(), inplace=True)
+            elif method == 'mean':
+                self.dataframe[col].fillna(self.dataframe[col].mean(), inplace=True)
+    
+    def identify_skewed_columns(self, threshold=0.75):
+        # Identify columns with skewness above the threshold
+        skewed_cols = self.dataframe.skew().abs() > threshold
+        return skewed_cols[skewed_cols].index.tolist()
+
+    def transform_skewed_columns(self, columns):
+        # Transform skewed columns using appropriate methods
+        for col in columns:
+            self.dataframe[col] = self.dataframe[col].apply(lambda x: x ** 0.5)  # Example transformation, use appropriate methods
+        
+    def remove_outliers(self, columns):
+        # Remove outliers from specified columns
+        for col in columns:
+            # Code to remove outliers (through using z-score )
+            # Removing outliers using Z-score method
+            z_scores = np.abs((self.dataframe[col] - self.dataframe[col].mean()) / self.dataframe[col].std())
+            self.dataframe = self.dataframe[(z_scores < 3)]
+            pass
+
+    def identify_highly_correlated_columns(self, threshold=0.8):
+        # Compute correlation matrix and identify highly correlated columns
+        corr_matrix = self.dataframe.corr().abs()
+        upper = corr_matrix.where(~pd.np.tril(pd.np.ones(corr_matrix.shape), k=-1).astype(pd.np.bool))
+        to_drop = [column for column in upper.columns if any(upper[column] > threshold)]
+        return to_drop
+
+    def remove_highly_correlated_columns(self, columns):
+        # Remove highly correlated columns
+        self.dataframe.drop(columns=columns, inplace=True)
---
+++ plotter.py
@@ -0,0 +1,36 @@
+
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+class Plotter:
+    @staticmethod
+    def visualize_data(dataframe):
+        # Histograms for numerical columns
+        numerical_cols = dataframe.select_dtypes(include=['int64', 'float64']).columns.tolist()
+        for col in numerical_cols:
+            plt.figure(figsize=(8, 6))
+            sns.histplot(data=dataframe, x=col, kde=True)
+            plt.title(f'Histogram of {col}')
+            plt.xlabel(col)
+            plt.ylabel('Frequency')
+            plt.show()
+
+        # Boxplots for numerical columns
+        for col in numerical_cols:
+            plt.figure(figsize=(8, 6))
+            sns.boxplot(data=dataframe, y=col)
+            plt.title(f'Boxplot of {col}')
+            plt.ylabel(col)
+            plt.show()
+
+        # Count plots for categorical columns
+        categorical_cols = dataframe.select_dtypes(include=['object', 'category']).columns.tolist()
+        for col in categorical_cols:
+            plt.figure(figsize=(8, 6))
+            sns.countplot(data=dataframe, x=col)
+            plt.title(f'Countplot of {col}')
+            plt.xlabel(col)
+            plt.ylabel('Count')
+            plt.xticks(rotation=45)
+            plt.show()
